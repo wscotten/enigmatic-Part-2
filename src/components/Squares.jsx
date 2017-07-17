964493776cc6ad80b1f6979e7d699fdfd9e0d24c
William <wscotten@gmail.com>
2017-07-16 23:06:01 -0700

    added animation of squares

--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 import {HiddenSquare} from './HiddenSquare';
 import {VisibleSquare} from './VisibleSquare';
 
+const solutionPaths = [
+  [40, 33, 26, 19, 18, 17, 16, 15, 8],
+  [40, 39, 38, 37, 36, 29, 22, 23, 24, 25, 26, 19, 12, 11, 10, 9, 8],
+  [40, 39, 38, 31, 24, 25, 18, 11, 10, 9, 8],
+  [40, 33, 26, 19, 12, 11, 10, 17, 24, 23, 22, 15, 8],
+  [40, 33, 26, 25, 24, 31, 38, 37, 36, 29, 22, 15, 8, 9, 10, 11, 12],
+  [40, 39, 32, 31, 24, 23, 16, 9, 8],
+  [40, 39, 38, 37, 36, 29, 22, 15, 8, 9, 10, 11, 12],
+  [40, 33, 26, 19, 12, 11, 10, 17, 24, 31, 30, 29, 22, 15, 8],
+  [40, 39, 32, 31, 24, 17, 18, 11, 12],
+  [40, 39, 38, 37, 36, 29, 22, 23, 24, 17, 10, 9, 8],
+  [40, 39, 32, 25, 24, 23, 22, 15, 8, 9, 10, 11, 12],
+  [40, 39, 38, 37, 36, 29, 22, 23, 24, 17, 10, 11, 12],
+  [40, 39, 38, 37, 36, 29, 22, 23, 24, 25, 26, 19, 12],
+];
+
+const solutionPath = solutionPaths[Math.floor(Math.random() * (13))];
+
 export default function Squares({path}) {
   const blocks = [];
   for(let i = 0; i < 49; i++) {
@@ -15,9 +33,22 @@ export default function Squares({path}) {
         );
       }
     }
-    return (
-      <VisibleSquare delay={1.2 - Math.floor(block / 7) / 5} key={block.toString()} />
-    );
+    const startingSolutionDelay = solutionPath.indexOf(block);
+    if(startingSolutionDelay >= 0) {
+      return (
+        <VisibleSquare
+          delay={startingSolutionDelay / 5}
+          key={block.toString()}
+        />
+      );
+    } else {
+      return (
+        <VisibleSquare
+          delay={(1.2 - Math.floor(block / 7) / 5) + solutionPath.length / 5}
+          key={block.toString()}
+        />
+      );
+    }
   });
   return (
     <div style={{
